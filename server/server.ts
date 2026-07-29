bd90b1065246d9aeb6de470864d47e3d3ac27d77
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-07-29 10:46:17 -0400

    first commit

--- server/server.ts
+++ server/server.ts
@@ -8,25 +8,38 @@ import bookingRouter from './routes/bookingRoutes.js'
 import ownerRouter from './routes/ownerRoutes.js'
 import adminRouter from './routes/adminRoutes.js'
 const app = express()
+// app.use(cors({
+//     origin: ['https://mernappecommerce-dashboard-3hh2bvrlo-carlos-projects-b4082f29.vercel.app', 'http://localhost:5000/api'],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
 
 
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
+    // Request methods you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
-// const corsOptions = {
-//     origin: 'https://mernappecommerce-dashboard-git-9efd06-carlos-projects-b4082f29.vercel.app',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true, // Allow cookies if needed
-//     optionsSuccessStatus: 204
-// };
+    // Request headers you wish to allow
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
 
-// // Use CORS middleware BEFORE your routes
-// app.use(cors(corsOptions))
-app.use(cors({
-    origin: ['https://mernappecommerce-dashboard-3hh2bvrlo-carlos-projects-b4082f29.vercel.app', 'http://localhost:5000/api'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // @ts-ignore
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 app.use('/api/auth', authRouter)
