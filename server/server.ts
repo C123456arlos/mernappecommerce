304ac95a4ac9f029eea6f6d15fd1074789cd1a2a
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-07-29 10:01:15 -0400

    first commit

--- server/server.ts
+++ server/server.ts
@@ -29,7 +29,7 @@ app.get('/', (req: Request, res: Response) => {
 // );
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "mernappecommerce-dashboard-ic1rvhtpu-carlos-projects-b4082f29.vercel.app")
+    res.header("Access-Control-Allow-Origin", "https://mernappecommerce-dashboard-ic1rvhtpu-carlos-projects-b4082f29.vercel.app")
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
