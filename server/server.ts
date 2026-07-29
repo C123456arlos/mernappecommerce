85e698de1a018d64fa767334ce3e8ca71678582b
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-07-29 10:06:28 -0400

    first commit

--- server/server.ts
+++ server/server.ts
@@ -107,25 +107,47 @@ app.use((req, res, next) => {
 
 
 
-const allowedOrigins = [
-    'https://mernappecommerce-dashboard-ae4v3yhrx-carlos-projects-b4082f29.vercel.app',
-    'https://mernappecommerce-dashboard-git-9efd06-carlos-projects-b4082f29.vercel.app',
-    'http://localhost:3000' // Include for local development
-];
+// const allowedOrigins = [
+//     'https://mernappecommerce-dashboard-ae4v3yhrx-carlos-projects-b4082f29.vercel.app',
+//     'https://mernappecommerce-dashboard-git-9efd06-carlos-projects-b4082f29.vercel.app',
+//     'http://localhost:3000' // Include for local development
+// ];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // Allow requests with no origin (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+
+//         if (allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     credentials: true // Allow cookies or authorization headers if needed
+// }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true // Allow cookies or authorization headers if needed
+    origin: 'https://mernappecommerce-dashboard-git-9efd06-carlos-projects-b4082f29.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
 }));
+
 
 
 app.use('/api/auth', authRouter)
